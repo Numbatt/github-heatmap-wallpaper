e20010e6bed0228477f10b5e2342ce5b8825cc65
Diego Rico <diego.rico10010@gmail.com>
2026-05-02 05:56:11 -0500

    feat(formula): pin v0.1.0 sha256

--- Formula/gh-wallpaper.rb
+++ Formula/gh-wallpaper.rb
@@ -1,10 +1,9 @@
 class GhWallpaper < Formula
   desc "GitHub contribution heatmap as your macOS desktop wallpaper"
   homepage "https://github.com/Numbatt/github-heatmap-wallpaper"
-  # TODO: before tagging v0.1, replace url + sha256 + version with the release tarball.
   url "https://github.com/Numbatt/github-heatmap-wallpaper/archive/refs/tags/v0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "9d5dc82adc35cc1011ce6ddf88163ce78d0abfa9e2a25d398ed36a82cd2fdaf2"
   license "MIT"
 
   head "https://github.com/Numbatt/github-heatmap-wallpaper.git", branch: "main"
