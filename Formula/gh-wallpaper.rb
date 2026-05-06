4181f6a456db4c95444b22c8292c32681cd8adca
Diego Rico <diego.rico10010@gmail.com>
2026-05-05 22:58:33 -0500

    feat(formula): bottles for v0.2.1

--- Formula/gh-wallpaper.rb
+++ Formula/gh-wallpaper.rb
@@ -8,6 +8,13 @@ class GhWallpaper < Formula
 
   head "https://github.com/Numbatt/github-heatmap-wallpaper.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Numbatt/github-heatmap-wallpaper/releases/download/v0.2.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b22a58c638f0fa24e9233c0ce84c0a722bc45b9474220260581d1009b529440b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "635d46f4fac987518f2e311c0138bcb5a7a805c105871498d7f7489cc2c935e3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f98e58bafd58828ad663f83b7c12888a3633ef52278be87f1eb748d036edf05"
+  end
+
   # Bottle block intentionally empty until release tag + bottles are built.
   # `script/update-bottle-block.sh v0.2.1` will splice in the real values
   # once CI publishes the Sonoma/Sequoia bottles and the manual Tahoe
